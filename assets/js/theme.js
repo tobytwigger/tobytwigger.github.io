fbb0ac5095e813a004a11b73339617a9e120c732
Toby Twigger <tobytwigger1@gmail.com>
2025-08-14 14:55:33 +0100

    Minor accessibility and CV updates

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,48 +1,49 @@
 const STORAGE_KEY = "theme";
-const THEME_ATTR  = "data-theme";
-const QUERY_KEY   = "(prefers-color-scheme: dark)";
+const THEME_ATTR = "data-theme";
+const QUERY_KEY = "(prefers-color-scheme: dark)";
 
 const themes = {
-  LIGHT: "light",
-  DARK: "dark",
+    LIGHT: "light",
+    DARK: "dark",
 };
 
 initTheme();
 
 function initTheme() {
-  const savedTheme = localStorage.getItem(STORAGE_KEY);
+    const savedTheme = localStorage.getItem(STORAGE_KEY);
 
-  if (savedTheme) {
-    // Storage theme
-    setTheme(savedTheme);
-  } else if (window.matchMedia && window.matchMedia(QUERY_KEY).matches) {
-    // system theme
-    setTheme(themes.DARK);
-  } else {
-    // Default theme
-    setTheme(themes.LIGHT);
-  }
+    if (savedTheme) {
+        // Storage theme
+        setTheme(savedTheme);
+    } else if (window.matchMedia && window.matchMedia(QUERY_KEY).matches) {
+        // system theme
+        setTheme(themes.DARK);
+    } else {
+        // Default theme
+        setTheme(themes.LIGHT);
+    }
 
-    window.onload = function() {
-      updateThemeImages(getTheme())
+    window.onload = function () {
+        updateThemeImages(getTheme())
+        updateDarkToggles(getTheme())
     };
 
-  // Watch for system theme changes
-  window.matchMedia(QUERY_KEY).addEventListener("change", (e) => {
-    const newTheme = e.matches ? themes.DARK : themes.LIGHT;
-    setTheme(newTheme);
-  });
+    // Watch for system theme changes
+    window.matchMedia(QUERY_KEY).addEventListener("change", (e) => {
+        const newTheme = e.matches ? themes.DARK : themes.LIGHT;
+        setTheme(newTheme);
+    });
 }
 
 function toggleTheme() {
-  const theme = getTheme();
-  const newTheme = theme === themes.DARK ? themes.LIGHT : themes.DARK;
-  setTheme(newTheme);
-  localStorage.setItem(STORAGE_KEY, newTheme);
+    const theme = getTheme();
+    const newTheme = theme === themes.DARK ? themes.LIGHT : themes.DARK;
+    setTheme(newTheme);
+    localStorage.setItem(STORAGE_KEY, newTheme);
 }
 
 function getTheme() {
-  return document.documentElement.getAttribute(THEME_ATTR);
+    return document.documentElement.getAttribute(THEME_ATTR);
 }
 
 function updateThemeImages(theme) {
@@ -58,8 +59,20 @@ function updateThemeImages(theme) {
     });
 }
 
+function updateDarkToggles(theme) {
+    if (theme === themes.DARK) {
+        document.querySelectorAll('.dark-toggle').forEach(toggle => {
+            toggle.classList.remove('dark'); // We want 'dark' to be active if it is NOT dark mode, so that it shows darker colors for a lighter background
+        });
+    } else {
+        document.querySelectorAll('.dark-toggle').forEach(toggle => {
+            toggle.classList.add('dark');
+        });
+    }
+}
+
 function setTheme(value) {
-  document.documentElement.setAttribute(THEME_ATTR, value);
-  console.log('set');
-  updateThemeImages(value);
+    document.documentElement.setAttribute(THEME_ATTR, value);
+    updateThemeImages(value);
+    updateDarkToggles(value);
 }
--- pages/cv.md
+++ pages/cv.md
@@ -1,30 +1,7 @@
 ---
 layout: page
-title: Resume
-permalink: /resume/
+title: CV
+permalink: /cv/
 weight: 3
-external_url: "/assets/docs/Resume - Toby Twigger.pdf"
+external_url: "/assets/docs/CV - Toby Twigger.pdf"
 ---
-
-# **About Me**
-
-Hi I am **{{ site.author.name }}** :wave:,<br>
-
-
-I'm a full-stack developer with a passion for building robust, scalable systems that solve real-world problems. Over 
-the years, I’ve led projects ranging from bespoke ERP systems to public-facing platforms, consistently delivering 
-clean, maintainable code that empowers teams and improves operations. Whether architecting a sales forecasting tool or 
-contributing to open-source Laravel packages, I take pride in thoughtful design, test coverage, and documentation. 
-My work spans early-stage startups to established organisations, and I thrive in collaborative, fast-moving environments where I can help shape both product and process.
-
-Outside of coding, I’m deeply motivated by projects that contribute to the public good - particularly those that encourage sustainable transport and help more people access nature. I’m an avid cyclist and hiker, and I bring that same sense of exploration and persistence to my development work. I care about making technology approachable, effective, and inclusive, and I’m always looking to work with teams who share a mission-driven ethos. Whether I’m integrating AI to streamline support workflows or debugging under a headlamp on a camping trip, I aim to leave systems - and the world - a little better than I found them.
-
-<div class="row">
-{% include about/skills.html title="Languages" source=site.data.programming-skills color="success" %}
-{% include about/skills.html title="Frameworks" source=site.data.frameworks color="info" %}
-{% include about/skills.html title="Tools" source=site.data.other-skills color="danger" %}
-</div>
-
-<div class="row">
-{% include about/timeline.html %}
-</div>
